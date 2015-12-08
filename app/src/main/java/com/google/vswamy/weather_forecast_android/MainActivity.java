820cf4a50775fbfe485391d515e4044964c41e21
Virupaksha Swamy U M <vswamy@outlook.com>
2015-12-07 17:22:44 -0800

    phase 1

--- app/src/main/java/com/google/vswamy/weather_forecast_android/MainActivity.java
+++ app/src/main/java/com/google/vswamy/weather_forecast_android/MainActivity.java
@@ -1,35 +1,70 @@
 package com.google.vswamy.weather_forecast_android;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity
 {
+    protected void registerEvents()
+    {
+        final Button button = (Button) findViewById(R.id.about);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent aboutIntent = new Intent(MainActivity.this, AboutActivity.class);
+                MainActivity.this.startActivity(aboutIntent);
+            }
+        });
+
+
+        final Button search = (Button) findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener()
+        {
+            private String validate()
+            {
+                return "Swamy yay";
+            }
+
+            @Override
+            public void onClick(View v)
+            {
+                String message = validate();
+                TextView textView = (TextView) findViewById(R.id.error_message);
+                if(message == null)
+                {
+                    // fetch data
+                    textView.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    textView.setVisibility(View.VISIBLE);
+                    textView.setText(message);
+                }
+            }
+        });
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        registerEvents();
     }
 
     @Override
